0655548b5bad08aebd034c0d42e8ccf97aeacd11
Gino Bernardi <gbernardi@cars.com>
2017-04-24 17:17:33 -0500

    render first three static todos

--- src/App.js
+++ src/App.js
@@ -2,17 +2,53 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let todos = [
+  {
+    todoTitle: "My first todo",
+    todoResponsible: "Gino",
+    todoDescription: "description",
+    todoPriority: "low"
+  },
+  {
+    todoTitle: "second todo",
+    todoResponsible: "Gino",
+    todoDescription: "description",
+    todoPriority: "low"
+  },
+  {
+    todoTitle: "My third todo",
+    todoResponsible: "Gino",
+    todoDescription: "description",
+    todoPriority: "low"
+  }
+];
+
 class App extends Component {
+// constructor props should get passed to parent sonstructor using super
+  constructor(props) {
+    super(props);
+
+// this makes the todos array available in the state object of this component
+    this.state = {
+      todos
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h4>Todo Count: <span className="badge">{this.state.todos.length}</span></h4>
+        <ul className="list-group">
+          {this.state.todos.map((todo, index) =>
+            <li className="list-group-item" key={index}>
+              <h4 className="list-group-item-heading">{todo.todoTitle}<small><span className="label label-info">{todo.todoPriority}</span></small></h4>
+              <p><span className="glyphicon glyphicon-user"></span>{todo.todoResponsible}</p>
+              <p>{todo.todoDescription}</p>
+
+              <button className="btn btn-danger btn-sm" onClick={this.handleRemoveTodo.bind(this, index)}><span className="glyphicon glyphicon-trash"></span>Delete</button>
+            </li>
+          )}
+        </ul>
       </div>
     );
   }
